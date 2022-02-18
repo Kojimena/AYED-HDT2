c49fccb3f579cb8b19ba056e8152f942bf005295
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2022-02-18 09:19:25 -0600

    Calc implementation

--- MyCalculadora.java
+++ MyCalculadora.java
@@ -1,3 +1,10 @@
-public class MyCalculadora {
+/**
+ * @author Jimena Hernández
+ * Clase que implementa la calculadora
+ * @version 19/02/2022
+ */
+
+public class MyCalculadora implements Calculadora {
+    
     
 }
