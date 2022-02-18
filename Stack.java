a35ec8ce753e861e1e26f5728b731c331e23c8ca
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2022-02-18 00:21:30 -0600

    Stack

--- Stack.java
+++ Stack.java
@@ -1,3 +1,27 @@
-public class Stack {
+/**
+ * @author Jimena Hernández
+ * Clase Stack
+ * Interfaz del stack
+ */
+
+ public interface Stack <E>{
+     
+    /**
+     * 
+     * @param elemento
+     */
+	public void Push (E elemento);
+
+    /**
+     * 
+     * @return
+     */
+	public E Pop();
+
+    /**
+     * 
+     * @return
+     */
+	public boolean IsEmpty();
     
 }
