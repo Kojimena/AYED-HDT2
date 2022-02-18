4b0a7beef226d7e7bd2dd807970150bf4379c83a
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2022-02-18 09:33:02 -0600

    implementation calculadora

--- MyCalculadora.java
+++ MyCalculadora.java
@@ -5,6 +5,54 @@
  */
 
 public class MyCalculadora implements Calculadora {
-    
-    
+
+    private Stack miStack;
+
+    public MyCalculadora(){
+        miStack= new MyStack();
+    }
+
+    /**
+     * @param vector
+     * @return res 
+     */
+    public int Calculo(String vector){
+        int res = 0;
+        String[] partes = vector.split(" "); //Separación de dígitos y operandos por medio de un espacio en blanco
+        for (int i = 0; i < partes.length; i++) {
+            try{
+                miStack.Push(Integer.parseInt(partes[i])); //Push si es un número  Ref: https://w3api.com/Java/Integer/parseInt/
+            
+            } catch (Exception e) {
+                switch(partes[i]){   //Si es un operador pasa al catch y se resuelve para el caso necesario
+                    
+                    //si operador es +
+                    case "+": 
+                        res = (int)miStack.Pop()+(int)miStack.Pop();
+                        miStack.Push(res);
+                    break;
+                    //si operador es -
+                    case "-": 
+                        res = (int)miStack.Pop()-(int)miStack.Pop();
+                        miStack.Push(res);
+                    break;
+                    //si operador es *
+                    case "*":
+                        res = (int)miStack.Pop()*(int)miStack.Pop();
+                        miStack.Push(res);
+                    break;
+                    //si operador es /
+                    case "/": 
+                        res = (int)miStack.Pop()/(int)miStack.Pop();
+                        miStack.Push(res);
+                    break;
+                }
+            }
+            
+        }
+        return res;
+
+    }
+
+
 }
