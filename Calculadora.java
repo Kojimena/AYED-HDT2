ebf3d4fab78cfdb69aff5f5c3393a57c6ce04e38
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2022-02-18 09:17:07 -0600

    calc

--- Calculadora.java
+++ Calculadora.java
@@ -1,3 +1,16 @@
-public class Calculadora {
+/**
+ * @author Jimena Hernández
+ * Interfaz calculadora
+ * @version 19/02/2022
+ */
+
+public interface Calculadora {
     
+    /**
+     * 
+     * @param vec
+     * @return
+     */
+    public int Calculo(String vec);
+
 }
